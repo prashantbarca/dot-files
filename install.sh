cce3e995b5012e7a58fb859c3f3e8c9c7a9c6231
Prashant Anantharaman <prashantbarca@users.noreply.github.com>
2017-08-07 14:50:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp -r dot-emacs.d/ ~/.emacs.d/
 
 cp dot-vimrc ~/.vimrc
 unzip dot-vim
-rm ~/.vim
+rm -rf ~/.vim
 mv .vim ~/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
