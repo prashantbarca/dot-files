51c5a6022a0e4c7049d607f5c640b90b2c778306
Prashant Anantharaman <prashant.barca@gmail.com>
2017-08-07 14:27:49 -0700

    Added vim and zsh

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ cd dot-files
 cp dot-emacs ~/.emacs
 cp -r dot-emacs.d/ ~/.emacs.d/
 
+cp dot-vimrc ~/.vimrc
+unzip dot-vim
+rm ~/.vim
+mv .vim ~/
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 cd ..
 rm -rf dot-files
-
